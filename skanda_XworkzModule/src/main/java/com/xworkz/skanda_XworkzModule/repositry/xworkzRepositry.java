a1b7f15da1d09d31bba40b017775008c1912db89
Skanda M V <skandagowda0@gmail.com>
2025-08-17 00:16:05 +0530

    Save Operation

---
+++ skanda_XworkzModule/src/main/java/com/xworkz/skanda_XworkzModule/repositry/xworkzRepositry.java
@@ -0,0 +1,9 @@
+package com.xworkz.skanda_XworkzModule.repositry;
+
+import com.xworkz.skanda_XworkzModule.entity.xworkzEntity;
+
+import java.util.List;
+
+public interface xworkzRepositry {
+    void save(List<xworkzEntity> list);
+}
