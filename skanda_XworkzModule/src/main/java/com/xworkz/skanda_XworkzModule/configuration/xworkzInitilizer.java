67481ee297ce58425af23b24f6071ec65de230b1
Skanda M V <skandagowda0@gmail.com>
2025-08-18 13:25:09 +0530

    Save Operation

---
+++ skanda_XworkzModule/src/main/java/com/xworkz/skanda_XworkzModule/configuration/xworkzInitilizer.java
@@ -0,0 +1,21 @@
+package com.xworkz.skanda_XworkzModule.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class xworkzInitilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
