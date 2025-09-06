045dd2a71ad1b1675e4fa19a964d050488028b14
Skanda M V <skandagowda0@gmail.com>
2025-09-06 22:45:50 +0530

    Save Operation with Frontend to backend ,Creating SignUp and SignIn page

--- skanda_XworkzModule/src/main/java/com/xworkz/skanda_XworkzModule/controller/XworkzController.java
+++ skanda_XworkzModule/src/main/java/com/xworkz/skanda_XworkzModule/controller/XworkzController.java
@@ -144,8 +144,18 @@ public class XworkzController {
     @RequestMapping("/VerifyOTP")
     public String VerifyOTP(XworkzDTO xworkzDTO,Model model){
         System.out.println("Verify OTP Controller");
+        String value = xworkzServiceImp.verifyOTP(xworkzDTO.getUserEmail(), xworkzDTO.getOtpcode());
+        if(value.isEmpty()){
+            System.out.println("Email Id Is Not Exist");
+            model.addAttribute("error","Email-Id Will Not Be Exist");
+            return "forgotPassword";
+        }
+        else{
+            System.out.println("Email-Id Is Exist");
+            model.addAttribute("success","Successfully Verify");
+            return "passwordChange";
 
-        return "passwordChange";
+        }
     }
 
     @RequestMapping("/deleteAccount")
