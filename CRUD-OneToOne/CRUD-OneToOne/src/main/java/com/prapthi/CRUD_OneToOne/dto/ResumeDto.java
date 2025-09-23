662d288dad5c3270ea585a0bfd3016de72b8d1ff
Skanda M V <skandagowda0@gmail.com>
2025-09-23 15:13:21 +0530

    One-To-One relation in JPA

--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/dto/ResumeDto.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/dto/ResumeDto.java
@@ -1,5 +1,6 @@
 package com.prapthi.CRUD_OneToOne.dto;
 
+import com.prapthi.CRUD_OneToOne.entity.StudentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,7 @@ public class ResumeDto {
     private long id;
 
     private String context;
+
+    private StudentEntity student;
 
 }
--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/service/ResumeService.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/service/ResumeService.java
@@ -1,5 +1,49 @@
 package com.prapthi.CRUD_OneToOne.service;
 
+import com.prapthi.CRUD_OneToOne.dto.ResumeDto;
+import com.prapthi.CRUD_OneToOne.entity.ResumeEntity;
+import com.prapthi.CRUD_OneToOne.repositry.ResumeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ResumeService {
+
+    @Autowired
+    private ResumeRepository resumeRepository;
+
+    public ResumeDto resumeDto(ResumeEntity resumeEntity){
+        if(resumeEntity==null){
+            return null;
+        }
+        return resumeDto(resumeEntity.getStudent().getResumeEntity());
+    }
+
+    public ResumeEntity resumeEntity(ResumeDto resumeDto){
+        if(resumeDto==null){
+            return null;
+        }
+        return new ResumeEntity(resumeDto.getId(), resumeDto.getContext(),resumeDto.getStudent());
+    }
+
+    public ResumeDto toDto(ResumeEntity resumeEntity){
+        return new ResumeDto(resumeEntity.getId(),
+                resumeEntity.getContext(),
+                resumeDto(resumeEntity.getStudent().getResumeEntity()).getStudent());
+    }
+
+    public ResumeEntity toEntity(ResumeDto resumeDto){
+        ResumeEntity resumeEntity = new ResumeEntity();
+        resumeEntity.setId(resumeDto.getId());
+        resumeEntity.setContext(resumeDto.getContext());
+        resumeEntity.setStudent(resumeDto.getStudent());
+        return resumeEntity;
+    }
+
+    public ResumeDto save(ResumeDto resumeDto){
+               ResumeEntity resumeEntity = toEntity(resumeDto);
+               ResumeEntity save = resumeRepository.save(resumeEntity);
+               return toDto(save);
+    }
 }
 
