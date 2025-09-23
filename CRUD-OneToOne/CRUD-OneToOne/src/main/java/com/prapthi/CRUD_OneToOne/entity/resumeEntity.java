322b610d9fb221672b1dcc98a2423bf50175a5fb
Skanda M V <skandagowda0@gmail.com>
2025-09-23 12:43:26 +0530

    One-To-One relation in JPA

--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/entity/resumeEntity.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/entity/resumeEntity.java
@@ -1,4 +1,25 @@
 package com.prapthi.CRUD_OneToOne.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class resumeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String context;
+
+    @OneToOne
+    @JoinColumn(name="studentId", nullable = false)
+    @JsonIgnore
+    private studentEntity student;
 }
--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/entity/studentEntity.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/entity/studentEntity.java
@@ -1,4 +1,26 @@
 package com.prapthi.CRUD_OneToOne.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class studentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String studentName;
+
+    private String studentDept;
+
+    private String studentContactNumber;
+
+    @OneToOne(mappedBy = "")
+    private resumeEntity resumeEntity;
 }
