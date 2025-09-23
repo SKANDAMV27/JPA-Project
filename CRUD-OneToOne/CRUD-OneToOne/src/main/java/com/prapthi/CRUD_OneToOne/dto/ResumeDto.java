fa4138697f7abef265a1917cc8d52495d762fb7d
Skanda M V <skandagowda0@gmail.com>
2025-09-23 14:26:47 +0530

    One-To-One relation in JPA

--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/dto/ResumeDto.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/dto/ResumeDto.java
@@ -9,4 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ResumeDto {
 
+    private long id;
+
+    private String context;
+
 }
--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/dto/StudentDto.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/dto/StudentDto.java
@@ -1,4 +1,17 @@
 package com.prapthi.CRUD_OneToOne.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDto {
+
+    private long id;
+    private String studentName;
+    private String studentDept;
+    private String studentContactNumber;
+    private ResumeDto resumeDto;
 }
