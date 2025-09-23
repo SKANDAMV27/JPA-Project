edec07794585b7120404ddee4da581b025eb9915
Skanda M V <skandagowda0@gmail.com>
2025-09-23 15:16:15 +0530

    One-To-One relation in JPA

--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/controller/ResumeController.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/controller/ResumeController.java
@@ -1,4 +1,24 @@
 package com.prapthi.CRUD_OneToOne.controller;
 
+import com.prapthi.CRUD_OneToOne.dto.ResumeDto;
+import com.prapthi.CRUD_OneToOne.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resume")
 public class ResumeController {
+
+    @Autowired
+    private ResumeService resumeService;
+
+
+    @PostMapping("/")
+    public ResumeDto saveAll(ResumeDto resumeDto){
+        System.out.println("Data Saved Successfully");
+
+        return resumeService.save(resumeDto);
+    }
 }
