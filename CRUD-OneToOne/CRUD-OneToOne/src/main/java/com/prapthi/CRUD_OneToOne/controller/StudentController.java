e31faaf6a2d93ebb26bf40b09647c19b20f797e5
Skanda M V <skandagowda0@gmail.com>
2025-09-23 14:51:52 +0530

    One-To-One relation in JPA

--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/controller/StudentController.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/controller/StudentController.java
@@ -1,4 +1,24 @@
 package com.prapthi.CRUD_OneToOne.controller;
 
+import com.prapthi.CRUD_OneToOne.dto.StudentDto;
+import com.prapthi.CRUD_OneToOne.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/")
+    public StudentDto saveAll(StudentDto studentDto){
+        System.out.println("Data Will Save Successfully");
+        return studentService.save(studentDto);
+    }
+
 }
--- CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/service/StudentService.java
+++ CRUD-OneToOne/CRUD-OneToOne/src/main/java/com/prapthi/CRUD_OneToOne/service/StudentService.java
@@ -1,5 +1,9 @@
 package com.prapthi.CRUD_OneToOne.service;
 
+import com.prapthi.CRUD_OneToOne.dto.ResumeDto;
+import com.prapthi.CRUD_OneToOne.dto.StudentDto;
+import com.prapthi.CRUD_OneToOne.entity.ResumeEntity;
+import com.prapthi.CRUD_OneToOne.entity.StudentEntity;
 import com.prapthi.CRUD_OneToOne.repositry.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +14,46 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
+
+    private ResumeDto toResumeDto(ResumeEntity resumeEntity) {
+        if (resumeEntity == null) {
+            return null;
+        }
+        return new ResumeDto(resumeEntity.getId(), resumeEntity.getContext());
+    }
+
+    private ResumeEntity toResumeEntity(ResumeDto resumeDto) {
+        if (resumeDto == null)
+            return null;
+        return new ResumeEntity(resumeDto.getId(), resumeDto.getContext(), null);
+    }
+
+
+    public StudentDto toDto(StudentEntity student){
+        return new StudentDto(
+                student.getId(),
+                student.getStudentName(),
+                student.getStudentDept(),
+                student.getStudentContactNumber(),
+                toResumeDto(student.getResumeEntity())
+        );
+    }
+
+    public StudentEntity toEntity(StudentDto studentDto){
+        StudentEntity student = new StudentEntity();
+         student.setId(studentDto.getId());
+        student.setStudentName(studentDto.getStudentName());
+        student.setStudentDept(studentDto.getStudentDept());
+        student.setStudentContactNumber(studentDto.getStudentContactNumber());
+        student.setResumeEntity(toResumeEntity(studentDto.getResumeDto()));
+        return student;
+    }
+
+    public StudentDto save(StudentDto studentDto){
+        StudentEntity studentEntity = toEntity(studentDto);
+        StudentEntity save = studentRepository.save(studentEntity);
+        return toDto(save);
+    }
 
 
 }
