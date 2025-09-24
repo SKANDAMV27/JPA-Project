2ee7bf32551708ced1d1fd59a4cdba64e6630262
Skanda M V <skandagowda0@gmail.com>
2025-09-24 12:04:25 +0530

    OneTOMany Relationship Project

--- CRUD-oneToMany/CRUD-oneToMany/src/main/java/com/prapthi/CRUD_oneToMany/controller/SchoolController.java
+++ CRUD-oneToMany/CRUD-oneToMany/src/main/java/com/prapthi/CRUD_oneToMany/controller/SchoolController.java
@@ -3,9 +3,7 @@ package com.prapthi.CRUD_oneToMany.controller;
 import com.prapthi.CRUD_oneToMany.dto.SchoolDto;
 import com.prapthi.CRUD_oneToMany.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -15,7 +13,7 @@ public class SchoolController {
     private SchoolService service;
 
     @PostMapping("/save")
-    public SchoolDto saveData(SchoolDto schoolDto){
+    public SchoolDto saveData(@RequestBody SchoolDto schoolDto){
         System.out.println("Data Save Successfully");
         return service.save(schoolDto);
 
