dd6e6fe5e77b019332b3d89abb528fdca14cd8a4
Skanda M V <skandagowda0@gmail.com>
2025-09-25 11:29:59 +0530

    Many-To-Many Relationship

--- CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/controller/StudentController.java
+++ CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/controller/StudentController.java
@@ -1,4 +1,20 @@
 package com.prapthi.CRUD_Many_To_Many.controller;
 
+import com.prapthi.CRUD_Many_To_Many.dto.StudentDTO;
+import com.prapthi.CRUD_Many_To_Many.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    public StudentDTO saveAll(StudentDTO studentDTO){
+        System.out.println("Save Successfully");
+        return studentService.save(studentDTO);
+    }
 }
--- CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/service/StudentService.java
+++ CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/service/StudentService.java
@@ -1,9 +1,16 @@
 package com.prapthi.CRUD_Many_To_Many.service;
 
+import com.prapthi.CRUD_Many_To_Many.dto.StudentDTO;
+import com.prapthi.CRUD_Many_To_Many.entity.StudentEntity;
+import com.prapthi.CRUD_Many_To_Many.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
-    pr
+    @Autowired
+    private StudentRepository studentRepository;
+
+
 }
