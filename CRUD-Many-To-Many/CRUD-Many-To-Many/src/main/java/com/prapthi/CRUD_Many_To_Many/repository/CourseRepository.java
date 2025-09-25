9920b650394d18fd1275d2e72ecf88b70557ad95
Skanda M V <skandagowda0@gmail.com>
2025-09-25 11:20:03 +0530

    Many-To-Many Relationship

--- CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/repository/CourseRepository.java
+++ CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.prapthi.CRUD_Many_To_Many.repository;
 
 import com.prapthi.CRUD_Many_To_Many.entity.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<CourseEntity,Integer> {
 }
