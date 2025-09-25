a313847f8cc38f28b4fceef053bd9f6df8aa7d91
Skanda M V <skandagowda0@gmail.com>
2025-09-25 12:09:09 +0530

    Many-To-Many Relationship

--- CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/controller/StudentController.java
+++ CRUD-Many-To-Many/CRUD-Many-To-Many/src/main/java/com/prapthi/CRUD_Many_To_Many/controller/StudentController.java
@@ -3,17 +3,20 @@ package com.prapthi.CRUD_Many_To_Many.controller;
 import com.prapthi.CRUD_Many_To_Many.dto.StudentDTO;
 import com.prapthi.CRUD_Many_To_Many.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
-    public StudentDTO saveAll(StudentDTO studentDTO){
+    @PostMapping("/save")
+    public StudentDTO saveAll(@RequestBody StudentDTO studentDTO){
         System.out.println("Save Successfully");
         return studentService.save(studentDTO);
     }
